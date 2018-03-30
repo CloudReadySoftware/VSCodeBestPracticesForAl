77ae78a8f769ba929c13e253e360e8e6c2255447
waldo <github@waldo.be>
2018-03-30 19:13:20 +0200

    Small changes

--- SQL/CheckAppData.sql
+++ SQL/CheckAppData.sql
@@ -1,8 +1,9 @@
 /* 
 To Connect:
 - Server: devsrv\SQLEXPRESS
+- Database: FinancialsUS
 - UID: sa
-- pwd: waldo1234
+- pwd: 
 */
 
 --List all tables
@@ -10,18 +11,14 @@ SELECT TABLE_NAME
 FROM INFORMATION_SCHEMA.TABLES 
 WHERE TABLE_TYPE = 'BASE TABLE'
 and TABLE_NAME LIKE '%b528ee70-8b63-4bd8-967c-402f9476b428%'
-UNION
-SELECT TABLE_NAME 
-FROM INFORMATION_SCHEMA.TABLES 
-WHERE TABLE_TYPE = 'BASE TABLE'
-and TABLE_NAME LIKE '%Just%Some%Table%'
+
 
 --List Data in tables
 SELECT TOP 10 *
-FROM [CRONUS International Ltd_$Customer$b528ee70-8b63-4bd8-967c-402f9476b428]
+FROM [CRONUS USA, Inc_$Customer$b528ee70-8b63-4bd8-967c-402f9476b428]
 
 SELECT TOP 10 *
-FROM [CRONUS International Ltd_$Just Some Table]
+FROM [CRONUS USA, Inc_$Just Some Table$b528ee70-8b63-4bd8-967c-402f9476b428]
 
 --Snippets (sql...)
 
@@ -29,4 +26,4 @@ FROM [CRONUS International Ltd_$Just Some Table]
 
 --Export Data (save as)
 
---Add Data / Re-publish
+
